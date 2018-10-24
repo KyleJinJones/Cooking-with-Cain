5e033612435b0080d8494eee0ac51eb60baf1c62
Conan Truong <truong.conan@gmail.com>
2018-10-23 17:39:35 -0700

    Attribute stuff

--- Cooking with Cain/Assets/Scripts/Deal_Damage.cs
+++ Cooking with Cain/Assets/Scripts/Deal_Damage.cs
@@ -7,5 +7,21 @@ public class Deal_Damage : MonoBehaviour {
 	public void damage(GameObject Attacker,GameObject target, int amt, string[] attributes) {
         target.GetComponent<Health>().damage(amt);
 
+        foreach (string attribute in attributes)
+        {
+            switch (attribute)
+            {
+                case "lifesteal":
+                    // do something
+                    break;
+                case "burn":
+                    // do something
+                    break;
+                case "stun":
+                    // do something
+                    break;
+                // etc
+            }
+        }
 	}
 }
