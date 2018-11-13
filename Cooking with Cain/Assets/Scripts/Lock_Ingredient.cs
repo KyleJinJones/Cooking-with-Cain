b90f649c0bfacc4e0de2e435950a937a5206c168
Kyle Jin Jones <kjjones1@uci.edu>
2018-11-12 16:13:03 -0800

    Added Ingredient Locking

---
+++ Cooking with Cain/Assets/Scripts/Lock_Ingredient.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lock_Ingredient : MonoBehaviour {
+    public int available_scene=0;
+	// Use this for initialization
+	void Start () {
+		if (SceneManager.GetActiveScene().buildIndex < available_scene)
+        {
+            this.gameObject.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
