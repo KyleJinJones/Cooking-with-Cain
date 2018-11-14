5b452801c83ed42dc40c4095bf2c38e78b8d6f64
Conan Truong <truong.conan@gmail.com>
2018-11-13 17:44:29 -0800

    Stun icons

--- Cooking with Cain/Assets/Scripts/Statuses.cs
+++ Cooking with Cain/Assets/Scripts/Statuses.cs
@@ -7,6 +7,7 @@ public class Statuses : MonoBehaviour {
     public GameObject burnIcon;
     public GameObject attackBoostIcon;
     public GameObject attackDebuffIcon;
+    public GameObject stunIcon;
 
     GameObject player;
     GameObject[] playerIcons;
@@ -24,17 +25,20 @@ public class Statuses : MonoBehaviour {
 
         enemyIcons = new GameObject[][] {
             new GameObject[] {
-            Object.Instantiate(burnIcon, positions[0] + new Vector3(-1, 2), Quaternion.identity),
-            Object.Instantiate(attackBoostIcon, positions[0] + new Vector3(0, 2), Quaternion.identity),
-            Object.Instantiate(attackDebuffIcon, positions[0] + new Vector3(1, 2), Quaternion.identity)},
+            Object.Instantiate(burnIcon, positions[0] + new Vector3(-1.5f, 2), Quaternion.identity),
+            Object.Instantiate(attackBoostIcon, positions[0] + new Vector3(-0.5f, 2), Quaternion.identity),
+            Object.Instantiate(attackDebuffIcon, positions[0] + new Vector3(0.5f, 2), Quaternion.identity),
+            Object.Instantiate(stunIcon, positions[0] + new Vector3(1.5f, 2), Quaternion.identity)},
             new GameObject[] {
-            Object.Instantiate(burnIcon, positions[1] + new Vector3(-1, 2), Quaternion.identity),
-            Object.Instantiate(attackBoostIcon, positions[1] + new Vector3(0, 2), Quaternion.identity),
-            Object.Instantiate(attackDebuffIcon, positions[1] + new Vector3(1, 2), Quaternion.identity)},
+            Object.Instantiate(burnIcon, positions[1] + new Vector3(-1.5f, 2), Quaternion.identity),
+            Object.Instantiate(attackBoostIcon, positions[1] + new Vector3(-0.5f, 2), Quaternion.identity),
+            Object.Instantiate(attackDebuffIcon, positions[1] + new Vector3(0.5f, 2), Quaternion.identity),
+            Object.Instantiate(stunIcon, positions[1] + new Vector3(1.5f, 2), Quaternion.identity)},
             new GameObject[] {
-            Object.Instantiate(burnIcon, positions[2] + new Vector3(-1, 2), Quaternion.identity),
-            Object.Instantiate(attackBoostIcon, positions[2] + new Vector3(0, 2), Quaternion.identity),
-            Object.Instantiate(attackDebuffIcon, positions[2] + new Vector3(1, 2), Quaternion.identity)}
+            Object.Instantiate(burnIcon, positions[2] + new Vector3(-1.5f, 2), Quaternion.identity),
+            Object.Instantiate(attackBoostIcon, positions[2] + new Vector3(-0.5f, 2), Quaternion.identity),
+            Object.Instantiate(attackDebuffIcon, positions[2] + new Vector3(0.5f, 2), Quaternion.identity),
+            Object.Instantiate(stunIcon, positions[2] + new Vector3(1.5f, 2), Quaternion.identity)}
         };
     }
 	
@@ -53,16 +57,16 @@ public class Statuses : MonoBehaviour {
         {
             if (enemies[j] == null)
             {
-                for (int i = 0; i < 3; i++)
+                for (int i = 0; i < 4; i++)
                 {
                     enemyIcons[j][i].SetActive(false);
                 }
             }
             else
             {
-                bool[] render2 = getStatuses(enemies[j]);
+                bool[] render2 = getStatusesEnemy(enemies[j]);
 
-                for (int i = 0; i < 3; i++)
+                for (int i = 0; i < 4; i++)
                 {
                     enemyIcons[j][i].SetActive(render2[i]);
                 }
@@ -73,5 +77,10 @@ public class Statuses : MonoBehaviour {
     bool[] getStatuses(GameObject entity)
     {
         return new bool[] { entity.GetComponent<Health>().burnDuration > 0, entity.GetComponent<Attack>().boostDuration > 0, entity.GetComponent<Attack>().debuffDuration > 0 };
+    }
+
+    bool[] getStatusesEnemy(GameObject entity)
+    {
+        return new bool[] { entity.GetComponent<Health>().burnDuration > 0, entity.GetComponent<Attack>().boostDuration > 0, entity.GetComponent<Attack>().debuffDuration > 0, entity.GetComponent<Enemy_Turn>().stunned };
     }
 }
