ff1490d07f76fd2dd45d3ebd49fccc520015dd59
Kyle Jin Jones <kjjones1@uci.edu>
2018-11-20 22:54:57 -0800

    Attack sfx

--- Cooking with Cain/Assets/Scripts/Ingredient_Selection.cs
+++ Cooking with Cain/Assets/Scripts/Ingredient_Selection.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Ingredient_Selection : MonoBehaviour {
     public List<Food> selected;
     public PlaySound deselect;
+    public PlaySound attack;
     // Use this for initialization
     void Start() {
 
@@ -27,6 +28,7 @@ public class Ingredient_Selection : MonoBehaviour {
 
     public void clear()
     {
+        attack.psound();
         selected.Clear();
     }
 
