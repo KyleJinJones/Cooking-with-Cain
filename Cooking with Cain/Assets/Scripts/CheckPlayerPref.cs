cca215f4213f1bf9b19a053a8e618acca6bf73a6
Kyle Jin Jones <kjjones1@uci.edu>
2018-11-29 17:34:20 -0800

    More UI updates

---
+++ Cooking with Cain/Assets/Scripts/CheckPlayerPref.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPlayerPref : MonoBehaviour {
+    public string keyname;
+    public int keyvalue;
+	// Used to restrict content the player does not have access to yet
+	void Awake () {
+        if (!(PlayerPrefs.HasKey(keyname) && PlayerPrefs.GetInt(keyname) == keyvalue))
+        {
+            this.gameObject.SetActive(false);
+        }
+	}
+}
