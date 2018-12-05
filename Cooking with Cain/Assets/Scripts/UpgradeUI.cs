d315e66000a2af0358d5e96e5a687ccfc2193b5e
Kyle Jin Jones <kjjones1@uci.edu>
2018-12-04 16:37:17 -0800

    Implemented Upgrade Scene Artwork and Boss Balance

---
+++ Cooking with Cain/Assets/Scripts/UpgradeUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeUI : MonoBehaviour {
+
+    public GameObject ui;
+    public Text uitext;
+    
+	// Use this for initialization
+	void Start () {
+        ui.SetActive(false);
+	}
+
+    public void Displayui(string s)
+    {
+        ui.SetActive(true);
+        uitext.text = s;
+    }
+    
+}
