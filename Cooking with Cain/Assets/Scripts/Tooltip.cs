7f1b41dc3a2e1a464de416cb5859e80fab6ddaa5
Conan Truong <truong.conan@gmail.com>
2019-01-16 01:03:07 -0800

    UI Tooltips

---
+++ Cooking with Cain/Assets/Scripts/Tooltip.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tooltip : MonoBehaviour
+{
+    Image image = null;
+    Text text = null;
+
+    private void Start()
+    {
+        image = GetComponentInChildren<Image>();
+        text = GetComponentInChildren<Text>();
+    }
+
+    void Update()
+    {
+        TooltipText hovered = TooltipText.hovered;
+
+        image.gameObject.SetActive(hovered != null);
+        text.gameObject.SetActive(hovered != null);
+
+        if (hovered != null)
+        {
+            text.text = hovered.GetText();
+            Canvas.ForceUpdateCanvases();
+            image.rectTransform.sizeDelta = text.rectTransform.sizeDelta + new Vector2(5, 5);
+            float sizeX = image.rectTransform.sizeDelta.x / 2;
+            float sizeY = image.rectTransform.sizeDelta.y / 2;
+
+            float posX = Input.mousePosition.x + sizeX;
+            float posY = Input.mousePosition.y + sizeY;
+
+            float camWidth = Camera.current.pixelWidth;
+            float camHeight = Camera.current.pixelHeight;
+
+            transform.position = new Vector3(Mathf.Min(posX, camWidth - sizeX), Mathf.Min(posY, camHeight - sizeY));
+        }
+    }
+}
---
+++ Cooking with Cain/Assets/Scripts/TooltipText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TooltipText : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public static TooltipText hovered = null;
+
+    [SerializeField]
+    [Multiline]
+    string text = "";
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        hovered = this;
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        hovered = null;
+    }
+
+    public string GetText()
+    {
+        return text;
+    }
+}
