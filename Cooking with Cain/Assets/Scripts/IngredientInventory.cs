ee70f2e52a3391fe7aafae413879bebcaa040cc1
Kyle Jin Jones <kjjones1@uci.edu>
2019-01-19 18:45:46 -0800

    Inventory Prototype

---
+++ Cooking with Cain/Assets/Scripts/IngredientInventory.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IngredientInventory : MonoBehaviour
+{
+    //handles each single ingredient instance in the inventory
+
+    [SerializeField] private IngredientManager igm;
+    public Ingredient ing;
+    public Image i;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        i.sprite = ing.sprite;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Selected()
+    {
+        Debug.Log("YO");
+        if (igm.ing1 ==null)
+        {
+            igm.ing1 = this;
+        }
+        else if (igm.ing2 == null)
+        {
+            igm.ing2 = this;
+        }
+        else if (igm.ing1 == this)
+        {
+            igm.ing1 = null;
+        }
+        else if (igm.ing2 == this)
+        {
+            igm.ing2 = null;
+        }
+        else
+        {
+            igm.ing1 = this;
+        }
+
+        if (igm.ing1 != null&&igm.ing2!=null)
+        {
+            igm.swapingredients();
+        }
+    }
+}
---
+++ Cooking with Cain/Assets/Scripts/IngredientManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//handles ingredient swapping, and creating a variable to pass to the battle scene for the selected ingredients
+public class IngredientManager : MonoBehaviour
+{
+    public List<IngredientInventory> loadout;
+    public List<IngredientInventory> Spare;
+    public IngredientInventory ing1=null;
+    public IngredientInventory ing2=null;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+    public void swapingredients()
+    {
+        Ingredient temp = ing1.ing;
+        ing1.ing = ing2.ing;
+        ing1.i.sprite = ing2.ing.sprite;
+        ing2.ing = temp;
+        ing2.i.sprite = temp.sprite;
+        ing1 = null;
+        ing2 = null;
+    }
+}
