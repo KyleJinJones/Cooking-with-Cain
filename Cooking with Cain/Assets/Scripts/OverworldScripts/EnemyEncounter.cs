39175fa3bc97e5ae7594fb0ffe2ae3c928cd1d2b
Conan Truong <truong.conan@gmail.com>
2019-01-30 15:44:05 -0800

    Create enemy encounter and enemy loader scripts

---
+++ Cooking with Cain/Assets/Scripts/OverworldScripts/EnemyEncounter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyEncounter : MonoBehaviour
+{
+    // The enemies that spawn in the battle scene
+    // Waiting for the enemy scritable object to be created
+    //public List<Enemy> enemies = new List<Enemy>();
+
+    // In case we have battles that don't start from colliding with an enemy
+    public bool encounterOnCollision;
+    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // [need change the tag to the tag used for the player]
+        if (encounterOnCollision && collision.tag == "")
+        {
+            StartEncounter();
+        }
+    }
+
+    public void StartEncounter()
+    {
+        // [need to save position, probably a static variable in the player movement script]
+
+        // Sets the enemy loader enemy list to this script's enemy list
+        //EnemyLoader.enemies = enemies;
+
+        // Loads the battle scene
+        SceneManager.LoadScene("Battle");
+    }
+}
