22af9ab2c8a980830b4d518717f75ab4f75368fa
Conan Truong <truong.conan@gmail.com>
2019-02-09 14:58:02 -0800

    Collision with enemy should go to battle scene

--- Cooking with Cain/Assets/Scripts/OverworldScripts/peaMovement.cs
+++ Cooking with Cain/Assets/Scripts/OverworldScripts/peaMovement.cs
@@ -97,7 +97,9 @@ public class peaMovement : MonoBehaviour
     
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Player") {
-            Debug.Log("Make something happen here. - peaMovement.cs");
+            EnemyEncounter encounter;
+            if ((encounter = GetComponent<EnemyEncounter>()) != null)
+                encounter.StartEncounter(collision.gameObject);
         }
     }
     
