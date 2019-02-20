1db5412a88ce9cff64fb7245e5b55852acd94013
Kyle Jin Jones <kjjones1@uci.edu>
2019-02-19 20:13:16 -0800

    Treasure Chest UI

--- Cooking with Cain/Assets/Scripts/OverworldScripts/Chest.cs
+++ Cooking with Cain/Assets/Scripts/OverworldScripts/Chest.cs
@@ -7,6 +7,8 @@ public class Chest : MonoBehaviour
     public UpgradeInfo reward;
     public Gold playergold;
     private bool open = false;
+    public GameObject treasurewindow;
+    [SerializeField] private Sprite openview;
     // Provides the Player with an item upon them opening it.
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -26,6 +28,11 @@ public class Chest : MonoBehaviour
                 playergold.UpdateGold((int)reward.upgradeamt);
             }
             open = true;
+            treasurewindow.SetActive(true);
+            treasurewindow.GetComponent<TreasureWindow>().treasureimage.sprite = reward.upgradeimage;
+            treasurewindow.GetComponent<TreasureWindow>().treasuretext.text = string.Format("Nice, You got {0}.", reward.infotext);
+            this.GetComponent<SpriteRenderer>().sprite = openview;
+
         }
     }
 }
