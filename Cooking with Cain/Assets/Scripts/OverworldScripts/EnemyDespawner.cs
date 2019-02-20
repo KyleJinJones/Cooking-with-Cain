3ab214d5f3ea59f522d61dc6b0edc31d37c2ffd7
Conan Truong <truong.conan@gmail.com>
2019-02-19 22:37:24 -0800

    Despawn IDs are now generated

--- Cooking with Cain/Assets/Scripts/OverworldScripts/EnemyDespawner.cs
+++ Cooking with Cain/Assets/Scripts/OverworldScripts/EnemyDespawner.cs
@@ -7,11 +7,22 @@ public class EnemyDespawner : MonoBehaviour
     // List of enemies that are defeated and won't spawn. Needs to be cleared when transitioning between areas. CT
     public static List<int> despawned = new List<int>();
 
+    // The ID to give the next enemy that loads. CT
+    public static int nextID = 0;
+    
     // ID of the enemy. CT
     public int enemyID;
 
+    void Awake()
+    {
+        nextID = 0;
+    }
+
     void Start()
     {
+        enemyID = nextID;
+        nextID++;
+
         if (despawned.Contains(enemyID))
         {
             Destroy(gameObject);
