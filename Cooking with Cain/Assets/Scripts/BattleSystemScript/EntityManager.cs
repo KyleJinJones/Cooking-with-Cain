92aabec03b13e68ea7d5550f42845262e9787478
Conan Truong <truong.conan@gmail.com>
2019-02-23 00:35:07 -0800

    Enemy remaining is more visible and only shows hidden enemies

--- Cooking with Cain/Assets/Scripts/BattleSystemScript/EntityManager.cs
+++ Cooking with Cain/Assets/Scripts/BattleSystemScript/EntityManager.cs
@@ -44,7 +44,7 @@ public class EntityManager : MonoBehaviour
 
     void Update()
     {
-        enemyRemaining.text = string.Format("Enemy Remaining: {0}", GetEnemyRemaining());
+        enemyRemaining.text = string.Format("Enemy Remaining: {0}", queue.Count);
     }
 
     public void PlayerAction(Ingredient[] ingredients)
