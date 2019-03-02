a9cacf12a7ca66d59f4c5e911cd8ea546520e4e0
Kyle Jin Jones <kjjones1@uci.edu>
2019-03-02 14:16:13 -0800

    Minor Organization+

---
+++ Cooking with Cain/Assets/Scripts/OverworldScripts/OverWorldUI/CameraFocus.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFocus : MonoBehaviour
+{
+    public GameObject player;
+ 
+
+    // Keeps the camera centered on a gameobject
+    void Update()
+    {
+        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, this.transform.position.z);
+    }
+}
