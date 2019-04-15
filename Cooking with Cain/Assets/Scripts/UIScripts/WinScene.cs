78f04980c76f4006563c4ea559e7ce2fba92cfe9
unknown <malbayat@uci.edu>
2019-04-15 12:25:34 -0700

    Added Credits button to winner scene

--- Cooking with Cain/Assets/Scripts/UIScripts/WinScene.cs
+++ Cooking with Cain/Assets/Scripts/UIScripts/WinScene.cs
@@ -22,4 +22,9 @@ public class WinScene : MonoBehaviour
         PlayerMovementFixed.spawnPosition = PlayerMovementFixed.checkpointPosition;
         SceneManager.LoadScene(lastScene);
     }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(2);
+    }
 }
