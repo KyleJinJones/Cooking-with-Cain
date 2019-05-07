8e5a9c9266eff545e54c4920529cf6f3de5ba56e
Kyle Jin Jones <kjjones1@uci.edu>
2019-05-07 16:40:04 -0700

    revamped ui

---
+++ Cooking with Cain/Assets/Scripts/UIScripts/ToggleMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleMenu : MonoBehaviour
+{
+    public GameObject menu;
+
+
+    public void Toggle()
+    {
+        menu.SetActive(!menu.activeSelf);
+    }
+}
