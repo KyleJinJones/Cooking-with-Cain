13bfe89e12a8cc5407e2dac585b2586074b09b2b
nuclearMonkeys <lyndond@uci.edu>
2019-05-10 13:26:11 -0700

    Added an AudioManager

---
+++ Cooking with Cain/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    static AudioManager instance  = null;
+    public int audioValue;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (instance != null)
+        {
+            Destroy(instance.gameObject);
+        }
+
+        instance = this;
+
+        audioSource = this.GetComponent<AudioSource>();
+        //audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>(); ;
+    }
+}
---
+++ Cooking with Cain/Assets/Scripts/UIScripts/SoundMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SoundMenu : MonoBehaviour
+{
+    public Slider sfxSlider;
+    public TMP_Text sfxValue;
+    public AudioManager audioManager;
+
+    // Update is called once per frame
+    private void Awake()
+    {
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+    }
+
+    void Update()
+    {
+        sfxValue.text = sfxSlider.value.ToString() + " :";
+        audioManager.audioValue = (int)sfxSlider.value;
+    }
+}
