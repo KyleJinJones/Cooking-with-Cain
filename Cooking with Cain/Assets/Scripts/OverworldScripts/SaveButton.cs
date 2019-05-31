d348a6903ce9f701109e94144064efd6554bb6ec
Muhammad Albayati <MuhammadAlbayati@dhcp-v045-226.mobile.uci.edu>
2019-05-31 13:52:57 -0700

    Changed artwork, made changes to tomato and UI prefabs

--- Cooking with Cain/Assets/Scripts/OverworldScripts/SaveButton.cs
+++ Cooking with Cain/Assets/Scripts/OverworldScripts/SaveButton.cs
@@ -7,6 +7,10 @@ public class SaveButton : MonoBehaviour
 {
     public PlayerMovementFixed player;
 
+    private void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovementFixed>();
+    }
+
     public void SavePositionAndScene()
     {
         SaveDataManager.currentData.currentPosition = player.transform.position;
