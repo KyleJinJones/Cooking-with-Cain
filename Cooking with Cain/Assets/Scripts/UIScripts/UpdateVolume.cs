81e7bad5eb14a352882fb1638b730079e9d0367b
Kyle Jin Jones <kjjones1@uci.edu>
2019-07-25 15:16:16 -0700

    Audio Sliders

---
+++ Cooking with Cain/Assets/Scripts/UIScripts/UpdateVolume.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateVolume : MonoBehaviour
+{
+    AudioManager a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
