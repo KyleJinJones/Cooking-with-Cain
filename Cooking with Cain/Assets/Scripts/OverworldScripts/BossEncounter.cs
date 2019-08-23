740d85318971d32f41fe3c7eae82cb9c97b7fc10
Conan Truong <truong.conan@gmail.com>
2019-08-22 20:20:17 -0700

    Changed enemy encounters of bosses to boss encounters

--- Cooking with Cain/Assets/Scripts/OverworldScripts/BossEncounter.cs
+++ Cooking with Cain/Assets/Scripts/OverworldScripts/BossEncounter.cs
@@ -6,13 +6,29 @@ using UnityEngine.SceneManagement;
 public class BossEncounter : EnemyEncounter
 {
     public List<GameObject> bosses = new List<GameObject>();
+    public string preBattleScene;
     public string winScene;
 
     public override void StartEncounter(GameObject player)
     {
         EnemyLoader.bosses = bosses;
-        base.StartEncounter(player);
-        EntityManager.overworldScene = winScene;
+        
+        // Copied from EnemyEncounter
+        EnemyLoader.enemies = enemies;
+        PlayerMovementFixed.spawnPosition = player.transform.position;
+        EntityManager.overworldScene = SceneManager.GetActiveScene().name;
+        EntityManager.overworldSceneLose = SceneManager.GetActiveScene().name;
+        EnemyDespawner despawner;
+        if ((despawner = GetComponent<EnemyDespawner>()) != null)
+            EnemyDespawner.despawned.Add(despawner.enemyID);
+
+        if (preBattleScene != "")
+            SceneManager.LoadScene(preBattleScene);
+        else
+            SceneManager.LoadScene("Battle");
+
+        if (winScene != "")
+            EntityManager.overworldScene = winScene;
         WinScene.lastScene = SceneManager.GetActiveScene().name;
     }
 }
